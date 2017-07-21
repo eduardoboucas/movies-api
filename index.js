c184c295affb42010a80e6a2c154e037696d284d
Eduardo Bouças <mail@eduardoboucas.com>
2017-07-21 23:47:08 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const API = require('./../static-api-generator')
+const api = new API({
+  blueprint: 'data/:genre/:year/:movie',
+  targetDirectory: 'output'
+})
+
+api.addEndpoint({
+  addIdToFiles: true,
+  depth: 1,
+  forEach: 'movie'
+})
+
+api.addEndpoint({
+  addIdToFiles: true,
+  depth: 2,
+  forEach: 'year'
+})
