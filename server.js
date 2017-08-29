fb536ea0e4c63e6aa66de8d2840ecf7916320413
Eduardo Bouças <mail@eduardoboucas.com>
2017-08-29 10:16:27 +0100

    Update endpoints

---
+++ server.js
@@ -0,0 +1,15 @@
+const API = require('./../static-api-generator')
+const api = new API({
+  blueprint: 'source/:language/:genre/:year/:movie',
+  targetDirectory: 'output'
+})
+
+api.generate({
+  endpoints: ['genre'],
+  levels: ['year', 'language'],
+  root: 'genre'
+})
+
+api.generate({
+  endpoints: ['movie']
+})
