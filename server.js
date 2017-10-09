75e356b5fbf77323a271dab9a9b4c96ef744dea3
Oleksandr <lesuk93@yahoo.com>
2017-10-09 21:06:59 +0300

    Replace targetDirectory with outputPath option

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const API = require('static-api-generator')
 const api = new API({
   blueprint: 'source/:language/:genre/:year/:movie',
-  targetDirectory: 'output'
+  outputPath: 'output'
 })
 
 api.generate({
